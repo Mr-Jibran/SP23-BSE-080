437dd9048a7c1a5bf0e439606abfd9faae048fd8
Mr-Jibran <156906766+Mr-Jibran@users.noreply.github.com>
2024-11-19 18:04:22 +0500

    Add files via upload

---
+++ Theory Assignment 3/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+let server = express();
+var layouts = require("express-ejs-layouts");
+
+server.set("view engine", "ejs");
+server.use(layouts);
+server.set('layout', 'ParentPage');
+server.use(express.static("public"));
+
+server.get("/",(req,res)=>{
+    return res.render("LayersIndex");
+})
+
+server.get("/contact", (req, res) =>{
+    return res.render("contact");
+});
+
+
+server.listen(5000,()=>{
+    console.log('server started at localhost:5000');
+});
